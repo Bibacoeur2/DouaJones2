7dfded4850db62f90ede6446dbf1592a522336e8
Bibacoeur2 <meryem.assila@gmail.com>
2015-02-28 10:46:45 +0100

    Update missionCheckin.js

--- js/missionCheckin.js
+++ js/missionCheckin.js
@@ -1,23 +1,158 @@
 $(document).on("pageinit", "#missionCheckin", function() {
-
-	idEntreprise:$('#entreprise_active').data('id_entreprise'),
-        sens:$('#select-custom-17').val(),					 
-
+var Entreprise = [];
+		Entreprise[0] = {Objet :GI,
+						 nom : "Total Logistics",
+						 Id=0,
+						 };
+		Entreprise[1] = {Objet :BMC,
+						 nom : "Banque Mondiale du Commerce",
+						 Id=1,
+						 };
+		Entreprise[2] = {Objet :GEN,
+						 nom : "Green Energy & Co", 
+						 Id=2,
+						 };
+		Entreprise[3] = {Objet :TC,
+						 nom : "Blue Telecom",
+						 Id=3,
+						 };
+		Entreprise[4] = {Objet :GE,
+						 nom : "Genius Electrics",
+						 Id=4,
+						 };
+		Entreprise[5] = {Objet :Rotonde,
+						 nom : "Brodewei", 
+						 Id=5,
+						 };
+		Entreprise[6] = {Objet :EFI,
+						 nom : "Hill Tone",
+						 Id=6,
+						 };
+						 
+		Entreprise[7] = {Objet :CB,
+						 nom : "Wilbedon & Co",
+						 Id=7,
+						 };
+						 
+		Entreprise[8] = {Objet :Humas,
+						 nom : "Center Park",
+						 Id=8,
+						 };
+						 
+		Entreprise[9] = {Objet :Kfet,
+						 nom : "Club Mad",
+						 Id=9,
+						 };
+						 
+		Entreprise[10] = {Objet :BS,
+						 nom : "Biotech",
+						 Id=10,
+						 };
+						 
+		Entreprise[11] = {Objet :IF,
+						 nom : "ITech",
+						  Id=11,
+						 };
+						 
+		Entreprise[12] = {Objet :Beurk,
+						 nom : "Miam Inc.",
+						 Id=12,
+						 };
+						 
+		Entreprise[13] = {Objet :CD,
+						 nom : "Campanule",
+						 Id=13,
+						 };
+						 
+		Entreprise[14] = {Objet :GCU,
+						 nom : "Leonard Construction",
+						 Id=14,
+						 };
+						 
+		Entreprise[15] = {Objet :GMC,
+						 nom : "Mech. Enterprise",
+						 Id=15,
+						 };
+						 
+		Entreprise[16] = {Objet :Dir,
+						 nom : "Doua Postal",
+						 Id=16,
+						 };
+						 
+		Entreprise[17] = {Objet :Capelle,
+						 nom : "Palais des Congrès",
+						 Id=17,
+						 };
+						 
+		Entreprise[18] = {Objet :CDS,
+						 nom : "Sport Center",
+						 Id=18,
+						 };
+						 
+		Entreprise[19] = {Objet :GJ,
+						 nom : "ORPIste",
+						 Id=19,
+						 };
+						 
+		Entreprise[20] = {Objet :GMD,
+						 nom : "Airboost",
+						 Id=20,
+						 };
+						 
+		Entreprise[21] = {Objet :Trav,
+						 nom : "Lion Formation",
+						 Id=21,
+						 }; 
+						 
+		Entreprise[22] = {Objet :Fermat,
+						 nom : "Pedro de Fermat",
+						 Id=22,
+						 }; 
+						 
+		Entreprise[23] = {Objet :batAB,
+						 nom : "Formula One",
+						 Id=23,
+						 };
+						 					 
+		Entreprise[24] = {Objet :gymB,
+						 nom : "Badison Square Garden",
+						 Id=24,
+						 };
+						 
+		Entreprise[25] = {Objet :SGM,
+						 nom : "SoTech Materials",
+						 Id=25,
+						 };
 	var socket=io.connect(adresse_serveur);
-		function checkin(entreprise) {
-			var teleportation = false;
-			//Si le joueur se trouve à la BMC et qu'il fait un checkin dans une société à laquelle il n'a pas accès, on met le booléen "teleportation" à vrai
-			if (google.maps.geometry.poly.containsLocation(position_joueur,Entreprise[1].Objet)) {
-				if (!(location_contained_or_edge (entreprise.Objet))) {
-					teleportation = true;
-				}
-			}
-			//On envoie les données du checkin
+		function checkin(Entreprise) {
+		
+			if($('#select-custom-100').val()== "Total Logistics") 
+         		//On envoie les données du checkin
 			socket.emit('setCheckin',{
-			idEntreprise:entreprise.index,
+			idEntreprise : 0, //si ca ne marche pas on met directement le chiffre de l'ID
 			idJoueur : idJoueur,
-			teleportation : teleportation
 			});
+			 else if($('#select-custom-100 option:selected').val()== "Banque Mondiale du Commerce") 
+         		
+					//On envoie les données du checkin
+					socket.emit('setCheckin',{
+					idEntreprise : 1,
+					idJoueur : idJoueur,
+					});
+				else if('#select-custom-100 option:selected').val()== "Green Energy & Co")
+						
+					//On envoie les données du checkin
+					socket.emit('setCheckin',{
+					idEntreprise : 2,
+					idJoueur : idJoueur,
+					});
+					else if('#select-custom-100 option:selected').val()== "Brodewei")
+						
+					//On envoie les données du checkin
+					socket.emit('setCheckin',{
+					idEntreprise : 5,
+					idJoueur : idJoueur,
+					});
 			
 		}
 		socket.on('resultSetCheckin', function (result) {
@@ -106,3 +241,24 @@ $(document).on("pageinit", "#missionCheckin", function() {
 //fin initialize
 }
 });
+// Envoyer demande au serveur pour récupérer les données nécessaures
+$(document).on("pageshow", "#Entreprise", function() {
+	var socket = io.connect(adresse_serveur);			
+	
+	socket.emit("getArgentDisponibleJoueur",idJoueur);
+	socket.emit("getCoursEntreprise", $('#entreprise_active').data('id_entreprise'));
+
+});
+
+
+// il faut lier le code au dessus à un bouton grâce à la fonction suivante:
+/*$(document).on("click","#btn_checkin", function(event) {
+	var socket=io.connect(adresse_serveur);
+		event.preventDefault();
+		event.stopImmediatePropagation();
+		$.mobile.changePage("#missionCheckin");
+		return false;
+	});*/
+	
+});
+ 
